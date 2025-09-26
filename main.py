ed9d225a2a5223ad7a42e8021bed52b1f06cbde4
ggrl <226671839+ggrl@users.noreply.github.com>
2025-09-26 09:54:32 +0200

    basic http forwarding
    Forwarding a simple HTTP-request from a python client.

---
+++ main.py
@@ -0,0 +1,38 @@
+import socket
+
+
+proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+proxy_socket.bind(('localhost', 8080))
+proxy_socket.listen(1)
+
+print("Server started...")
+
+while True:
+    
+    client_socket, client_address = proxy_socket.accept()
+    print(f"Client connected: {client_address}")
+    
+    
+    request = client_socket.recv(4096).decode()
+    print("2")
+    
+    
+    host = None
+    for line in request.split('\n'):
+        if line.startswith('Host:'):
+            host = line.split(' ')[1].strip()
+            break
+    
+    if host:
+        target_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        target_socket.connect((host, 80))
+        target_socket.send(request.encode())
+        
+        response = target_socket.recv(4096)
+        
+        client_socket.send(response)
+        
+        target_socket.close()
+    
+    client_socket.close()
+    print("response returned")
